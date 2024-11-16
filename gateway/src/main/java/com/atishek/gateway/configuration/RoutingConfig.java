6185f0551e28f88d05a8b6c3033bcf6c19833d67
ATISHEK <atishek007@gmail.com>
2024-11-16 09:36:10 +0530

    Added custom routing for microservices

---
+++ gateway/src/main/java/com/atishek/gateway/configuration/RoutingConfig.java
@@ -0,0 +1,27 @@
+package com.atishek.gateway.configuration;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RoutingConfig {
+
+    @Bean
+    public RouteLocator createCustomRoutes(RouteLocatorBuilder builder) {
+
+        return builder.routes().
+                route(p -> p
+                        .path("/api/accounts/**")
+                        .filters(f -> f.rewritePath("/api/accounts/(?<segment>.*)", "/${segment}"))
+                        .uri("lb://ACCOUNTS")
+                )
+                .route(p -> p.
+                        path("/api/loans/**")
+                        .filters(f -> f.rewritePath("/api/loanss/?<segment>.*", "/${segment}"))
+                        .uri("lb://LOANS"))
+                .build();
+    }
+
+}
